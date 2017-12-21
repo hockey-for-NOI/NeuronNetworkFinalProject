3d64b9d389da10b3456699009ad6f74bb34e74f7
Xembo <809642754@qq.com>
2017-12-21 16:45:08 +0800

    add regression folder and related codes
    "mlp.py" codes for mlp
    case_generator.py: codes for case generator

---
+++ Regression/case_generator.py
@@ -0,0 +1,38 @@
+import csv
+import numpy as np
+
+INPUT_FILE_NAME = 't_price.csv'
+csv_reader = csv.reader(open(INPUT_FILE_NAME))
+OUTPUT_FILE_NAME = 't_price_cut.csv'
+LABEL_FILE_NAME = 't_label.csv'
+
+flag = False
+data = []
+sample = []
+for row in csv_reader:
+	if flag == False:
+		flag = True
+		continue
+	data.append(row)
+data = np.array(data)
+data = data[:, 260: -241]
+for i in range(1211):
+	for j in range(155):
+		sample.append(data[i, j * 100: j * 100 + 300])
+
+sample = np.array(sample)
+csv_writer = csv.writer(open(OUTPUT_FILE_NAME, 'w'))
+csv_writer_2 = csv.writer(open(LABEL_FILE_NAME, 'w'))
+for i in range(sample.shape[0]):
+	csv_writer.writerow(list(sample[i]))
+print sample.shape
+
+for i in range(sample.shape[0]):
+	if (i < 50000):
+		csv_writer_2.writerow([float(sorted(sample[i])[int(200 * 0.75) - 1])])
+		continue
+	if (i < 100000):
+		csv_writer_2.writerow([float(sorted(sample[i])[int(200 * 0.50) - 1])])
+	else:
+		csv_writer_2.writerow([float(sorted(sample[i])[int(200 * 0.75) - 1])])
+		print float(sorted(sample[i])[int(200 * 0.75) - 1]) - float(sorted(sample[i])[int(200 * 0.50) - 1])
